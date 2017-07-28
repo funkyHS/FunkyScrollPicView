e029e4b009fed8856b9a538e7799f22c97c5ea2d
funkyHS <hs1024942667@163.com>
2017-07-28 15:05:32 +0800

    Initial commit

---
+++ Example/FunkyScrollPicView/HSViewController.m
@@ -0,0 +1,29 @@
+//
+//  HSViewController.m
+//  FunkyScrollPicView
+//
+//  Created by funkyHS on 07/28/2017.
+//  Copyright (c) 2017 funkyHS. All rights reserved.
+//
+
+#import "HSViewController.h"
+
+@interface HSViewController ()
+
+@end
+
+@implementation HSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
